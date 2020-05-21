613b522bb49ab2329f7251398a12e04ce696576f
Eelco de Gee <eelco.de.gee@outlook.ie>
2020-05-21 09:22:34 +0100

    Added new links to create and add links

--- kmdb/main/urls.py
+++ kmdb/main/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from main import views
 
 
+app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:cat_name>/', views.details, name='details')
+    path('addlink/', views.addlink, name='add_link'),
+    path('addcat/', views.addcategory, name='add_cat'),
+    path('<str:cat_name>/', views.details, name='details'),
+    
 ]
--- kmdb/main/views.py
+++ kmdb/main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import (render, get_object_or_404, redirect)
+from datetime import datetime
 
 from .models import Category, Item
 # Create your views here.
@@ -6,13 +7,30 @@ from .models import Category, Item
 
 def index(request):
     Categories = Category.objects.all()
-    # template = loader.get_template('index.html')
     context = {'Categories': Categories}
     return render(request, 'index.html', context)
 
 
 def details(request, cat_name):
     category = get_object_or_404(Category, cat_name=cat_name)
-    links = Item.objects.get(category=category.pk)
+    links = Item.objects.filter(category=category.pk)
     context = {'category': category, 'links': links}
     return render(request, 'links.html', context)
+
+
+def addlink(request):
+    if request.method == 'POST':
+        cat = Category.objects.get(cat_name=request.POST['category'])
+        time = datetime.now()
+        url = request.POST['Link']
+        item = Item(url_link=url, category=cat, data_added=time)
+        item.save()
+        return redirect('main:index')
+
+    categories = Category.objects.all()
+    context = {'categories': categories, }
+    return render(request, 'addlink.html', context)
+
+
+def addcategory(request):
+    return render(request, 'addcat.html')
